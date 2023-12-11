92309dff678f5199c97f1543356c272752e31916
Ádám Szeteli <adamszeteli@gmail.com>
2023-12-11 05:14:03 +0100

    test cases

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,6 +1,4 @@
 import 'package:indoor_localization_app/pages/map_page.dart';
-import 'package:indoor_localization_app/pages/map_page_old.dart';
-import 'package:indoor_localization_app/pages/scan_beacons.dart';
 
 var appRoutes = {
   '/map': (context) => const MapPage(),
